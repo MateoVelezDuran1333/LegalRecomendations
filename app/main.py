7cb5485f3af6e6450063bbe4a7a1a6417660b139
Mateo <mateo.vele1333@outlook.com>
2025-11-11 22:33:11 -0500

    Añadido cors

--- app/main.py
+++ app/main.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
 from app.api.endpoints import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title='Regresion API')
 app.include_router(router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # En producción, reemplaza "*" por tu dominio frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
